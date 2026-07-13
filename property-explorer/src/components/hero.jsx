e907217c3511eab54ee762983800736e005d8b2a
faseehullah404 <faseeh.ullah404@gmail.com>
2026-07-13 15:43:59 +0500

    Update hero.jsx

--- property-explorer/src/components/hero.jsx
+++ property-explorer/src/components/hero.jsx
@@ -1,6 +1,6 @@
 function hero(){
     return(
-        <div class = "grid grid-cols-[700px_400px] max-w-full  ">
+        <div class = "grid grid-cols-[700px_580px] max-w-full  ">
             <div >
                 <h1 className="text-5xl font-bold text-blue-950 leading-12">
                     Enhance Your Sales
@@ -8,7 +8,7 @@ function hero(){
                     <br />
                     Estate CRM in UAE
                 </h1>
-                <div className="mt-4 leading-7 text-lg translate-x-1">
+                <div className="mt-4 leading-8 text-lg translate-x-1">
                     <p>
                         Empower developers and real estate brokers by using cutting-edge tools to automate and streamline property management. By leveraging cutting-edge technologies like
                     <span className="font-bold"> Real Estate CRM in UAE</span>
@@ -24,6 +24,19 @@ function hero(){
                     </a>
                     
                 </div>
+            </div>
+            <div>
+                <form className="shadow-xl p-6 ">
+                    <div >
+                        <input className="mt-4 border w-full border-gray-100 rounded-4xl p-4" placeholder="Name" />
+                        <input className="mt-4 border w-full border-gray-100 rounded-4xl p-4" placeholder="Phone Number" />
+                        <input className="mt-4 border w-full border-gray-100 rounded-4xl p-4" placeholder="Email" />
+                        <input className="mt-4 border w-full border-gray-100 rounded-4xl p-4" placeholder="Describe Your Requirement" />
+                        <a href="#" className="mt-6 mb-16 inline-flex rounded-2xl bg-green-700 text-white hover:bg-white hover:text-black py-3 px-5 shadow-[0_0_30px_rgba(56,189,248,0.25)] text-center ">
+                        Submit
+                    </a>
+                    </div>
+                </form>
             </div>
         </div>
     )
