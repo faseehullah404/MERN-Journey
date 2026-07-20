188ce6c0092bc38573a06b0905728e74d40bff44
faseehullah404 <faseeh.ullah404@gmail.com>
2026-07-20 16:11:01 +0500

    Update services.jsx

--- property-explorer/src/components/services.jsx
+++ property-explorer/src/components/services.jsx
@@ -6,11 +6,11 @@ import icon2 from "../assets/icon2.png"
 function Services(){
     return(
         <div className=" bg-cyan-50  pb-20">
-            <h1 className="text-black  pt-10 text-5xl font-extrabold text-center">
+            <h1 className="text-black  pt-10 text-3xl md:text-5xl font-extrabold text-center">
                 Real Estate CRM in UAE For Property
             </h1>
 
-            <div className="grid grid-cols-3 gap-8 max-w-[1200px] mx-auto pt-10 ">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-[1200px] mx-auto pt-10 px-5">
             <Card_service
             icon={icon1}
             title={"Property Listing"}
