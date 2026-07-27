387fbd4501b482e6b1825d6aa1540c1fbca24ba1
faseehullah404 <faseeh.ullah404@gmail.com>
2026-07-27 15:43:56 +0500

    Add video section component
    Add Video component to the app with background video, semi-transparent overlay, and heading text. The component displays a full-width video section below the Hero section with responsive styling using Tailwind CSS.

--- i2c-ui-clone/src/App.jsx
+++ i2c-ui-clone/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import Hero from "./components/hero.jsx"
 import Nav from "./components/nav"
+import Video from "./components/videosection.jsx"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
     <>
     <Nav />
     <Hero />
+    <Video/>
     </>
   )
 }
--- i2c-ui-clone/src/components/videosection.jsx
+++ i2c-ui-clone/src/components/videosection.jsx
@@ -34,6 +34,26 @@ function Video(){
             previewVideo.removeEventListener("loadedmetadata", startVideos)
         }
         }, [])
+        return(
+            <section className="relative mt-[35px] h-[564px] w-full overflow-hidden bg-[#1B223A]">
+                <video
+                    ref={mainVideoRef}
+                    src={vid}
+                    autoPlay
+                    muted
+                    loop
+                    playsInline
+                    preload="auto"
+                    className="absolute inset-0 h-full w-full object-cover"/>
+                    <div className="absolute inset-0 bg-black/30" />
+                    <div className="absolute inset-y-100 inset-x-0">
+                        <div className="relative mx-auto h-full w-[1227px]">
+                            <h1 className="font-[300] font-['Inter'] text-[40px] text-white leading-12 tracking-[-4px]"> Deliver your program faster, more profitably and <br/> ensure a superior customer experience.</h1>
+                        </div>
+                    </div>
+            </section>
+            
+        )
 
 }
 export default Video
