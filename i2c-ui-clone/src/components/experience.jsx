043b72952bd7b1785dcfc12c0432a2cfe26809c5
faseehullah404 <faseeh.ullah404@gmail.com>
2026-08-04 20:45:08 +0500

    Update experience.jsx
    half mobile optimize

--- i2c-ui-clone/src/components/experience.jsx
+++ i2c-ui-clone/src/components/experience.jsx
@@ -115,77 +115,391 @@ function Experience() {
   return (
     
     
-    <section className="w-full bg-white  mt-11">
-        <div
+<section
+  className="
+    mt-[22px] w-full
+    overflow-x-hidden
+    bg-white pb-[100px]
+
+    md:mt-11
+    md:pb-[120px]
+  "
+>
+  <div
+    className="
+      mx-auto grid w-full
+      grid-cols-1
+      items-start gap-y-[55px]
+      px-[22px]
+
+      md:w-[1230px]
+      md:grid-cols-[430px_1fr]
+      md:gap-x-[70px]
+      md:gap-y-0
+      md:px-0
+    "
+  >
+    {/* ================================================= */}
+    {/* TOP CONTENT                                      */}
+    {/* Mobile: sabse pehle                              */}
+    {/* Desktop: right column                            */}
+    {/* ================================================= */}
+
+    <div
+      className="
+        order-1 min-w-0
+        translate-x-0
+
+        md:col-start-2
+        md:row-start-1
+        md:translate-x-6
+      "
+    >
+      {/* Main heading */}
+      <h2
         className="
-            mx-auto grid w-[1230px]
-            grid-cols-[430px_1fr]
-            items-start gap-[70px]
+          max-w-[330px]
+          font-['Inter']
+          text-[31px] font-[400]
+          leading-[1.01]
+          tracking-[-2px]
+          text-[#1B223A]
+
+          md:max-w-none
+          md:text-[48px]
+          md:leading-[1.15]
+          md:tracking-[-3px]
         "
+      >
+        Decades of experience drive
+
+        <br className="hidden md:block" />
+
+        innovation and speed to market
+
+        <Circle
+          aria-hidden="true"
+          className="
+            mb-[3px] ml-[2px]
+            inline-block
+            h-[5px] w-[5px]
+            fill-[#FF7200]
+            text-[#FF7200]
+
+            md:mb-[5px]
+            md:ml-[5px]
+            md:h-[7px]
+            md:w-[7px]
+          "
+          strokeWidth={0}
+        />
+      </h2>
+
+      {/* Benefit cards */}
+      <div
+        className="
+          -mr-[22px] mt-[30px]
+          flex gap-[13px]
+          overflow-x-auto
+          pb-[8px] pr-[22px]
+          [scrollbar-width:none]
+          [&::-webkit-scrollbar]:hidden
+
+          md:mr-0
+          md:mt-[35px]
+          md:grid
+          md:grid-cols-3
+          md:gap-[18px]
+          md:overflow-visible
+          md:pb-0
+          md:pr-0
+        "
+      >
+        {platformBenefits.map((benefit) => {
+          return (
+            <div
+              key={benefit.id}
+              className="
+                flex h-[175px]
+                min-w-[185px]
+                flex-col justify-between
+                rounded-[9px]
+                bg-[#F7FAFD]
+                px-[22px] py-[20px]
+                shadow-[0_4px_12px_rgba(27,34,58,0.08)]
+
+                md:h-[205px]
+                md:min-w-0
+                md:rounded-[18px]
+                md:px-[24px]
+                md:py-[14px]
+              "
+            >
+              <img
+                src={benefit.icon}
+                alt=""
+                className="
+                  h-[35px] w-[35px]
+                  object-contain
+
+                  md:h-[45px]
+                  md:w-[45px]
+                "
+              />
+
+              <p
+                className="
+                  font-['Inter']
+                  text-[14px] font-[400]
+                  leading-[17px]
+                  text-[#1B223A]
+
+                  md:-translate-y-4
+                  md:text-[20px]
+                  md:leading-[23px]
+                "
+              >
+                {benefit.text.map((line) => {
+                  return (
+                    <span key={line} className="block">
+                      {line}
+                    </span>
+                  )
+                })}
+              </p>
+            </div>
+          )
+        })}
+      </div>
+
+      {/* Supporting copy */}
+      <div className="mt-[28px] md:mt-[34px]">
+        <h3
+          className="
+            font-['Inter']
+            text-[17px] font-[500]
+            leading-[21px]
+            text-[#1B223A]
+
+            md:text-[24px]
+            md:leading-[28px]
+          "
         >
+          Responsive handling. Guided navigation.
+        </h3>
 
-        <aside className="sticky top-[80px] self-start mt-7 ">
-                    <div className="">
-                    <div className="relative h-[372px] w-[390px] ">
+        <p
+          className="
+            mt-[1px]
+            max-w-[320px]
+            font-['Inter']
+            text-[15px] font-[300]
+            leading-[19px]
+            tracking-[-0.4px]
+            text-[#1B223A]
 
-                        {platformSlides.map((slide, index) => {
-                        let slideStyle =
-                            "pointer-events-none opacity-0"
+            md:mt-0
+            md:max-w-none
+            md:text-[24px]
+            md:leading-[24px]
+            md:tracking-[-1px]
+          "
+        >
+          Built on a foundation of stability and for what's next.
+        </p>
+      </div>
 
-                        if (activeSlide === index) {
-                            slideStyle =
-                            "pointer-events-auto opacity-100"
-                        }
+      {/* Learn More button */}
+      <a
+        href="#"
+        className="
+          group mt-[30px]
+          flex h-[47px] w-[136px]
+          items-center justify-center
+          gap-[9px]
+          rounded-[7px]
+          border-2 border-[#1434CB]
 
-                        return (
-                            <div
-                            key={slide.id}
-                            className={`
-                                absolute inset-0
-                                transition-opacity duration-700 ease-in-out
-                                ${slideStyle}
-                            `}
-                            >
-                            <img
-                                src={slide.image}
-                                alt={slide.title}
-                                className="h-[318px] w-[383px] object-contain"
-                            />
+          font-['Inter']
+          text-[13px] font-[500]
+          tracking-[-0.4px]
+          text-[#1434CB]
 
-                            <h3
-                                className="
-                                absolute bottom-[73px] left-1/2
-                                -translate-x-1/2 whitespace-nowrap
-                                font-['Inter'] text-[32px]
-                                font-[500] leading-[34px]
-                                text-white
-                                "
-                            >
-                                {slide.title}
-                            </h3>
-                            </div>
-                        )
-                        })}
+          hover:bg-[#1434CB]
+          hover:text-white
 
-                    </div>
-                </div>
-                <div className="relative -translate-y-3 pl-[28px] translate-x-20 ">
+          md:mt-[32px]
+          md:h-[58px]
+          md:w-[170px]
+          md:gap-[12px]
+          md:text-[18px]
+          md:tracking-[-1px]
+        "
+      >
+        <span>Learn More</span>
 
-                {/* Ek hi dot hai jo selected item tak move karega */}
-                <span
-                    aria-hidden="true"
-                    className="
-                    absolute left-0 
-                    h-[9px] w-[9px]
-                    rounded-full bg-[#1434CB]
-                    transition-transform duration-500 ease-out
-                    translate-y-10 
-                    "
-                    style={{
-                    transform: `translateY(${activeSlide * 113}px)`,
-                    }}
+        <span
+          className="
+            relative h-[16px] w-[16px]
+            overflow-hidden
+
+            md:h-[19px]
+            md:w-[19px]
+          "
+        >
+          <ArrowUpRight
+            className="
+              absolute left-0 top-0
+              h-[16px] w-[16px]
+              transition-transform duration-300
+              group-hover:-translate-y-full
+
+              md:h-[19px]
+              md:w-[19px]
+            "
+            strokeWidth={1.5}
+          />
+
+          <ArrowUpRight
+            className="
+              absolute left-0 top-full
+              h-[16px] w-[16px]
+              transition-transform duration-300
+              group-hover:-translate-y-full
+
+              md:h-[19px]
+              md:w-[19px]
+            "
+            strokeWidth={1.5}
+          />
+        </span>
+      </a>
+    </div>
+
+    {/* ================================================= */}
+    {/* TRIANGLE SLIDER                                  */}
+    {/* Mobile: button ke baad                           */}
+    {/* Desktop: left sticky column                      */}
+    {/* ================================================= */}
+
+    <aside
+      className="
+        order-2
+        static mt-[8px]
+        w-full self-start
+
+        md:col-start-1
+        md:row-start-1
+        md:row-span-2
+        md:sticky
+        md:top-[80px]
+        md:mt-7
+      "
+    >
+      {/* Triangle */}
+      <div
+        className="
+          mx-auto w-full
+          max-w-[330px]
+
+          md:mx-0
+          md:max-w-none
+        "
+      >
+        <div
+          className="
+            relative h-[315px]
+            w-full
+
+            md:h-[372px]
+            md:w-[390px]
+          "
+        >
+          {platformSlides.map((slide, index) => {
+            let slideStyle =
+              "pointer-events-none opacity-0"
+
+            if (activeSlide === index) {
+              slideStyle =
+                "pointer-events-auto opacity-100"
+            }
+
+            return (
+              <div
+                key={slide.id}
+                className={`
+                  absolute inset-0
+                  transition-opacity
+                  duration-700 ease-in-out
+                  ${slideStyle}
+                `}
+              >
+                <img
+                  src={slide.image}
+                  alt={slide.title}
+                  className="
+                    h-[285px] w-full
+                    object-contain
+
+                    md:h-[318px]
+                    md:w-[383px]
+                  "
                 />
 
+                <h3
+                  className="
+                    absolute bottom-[57px]
+                    left-1/2
+                    -translate-x-1/2
+                    whitespace-nowrap
+
+                    font-['Inter']
+                    text-[25px] font-[500]
+                    leading-[28px]
+                    text-white
+
+                    md:bottom-[73px]
+                    md:text-[32px]
+                    md:leading-[34px]
+                  "
+                >
+                  {slide.title}
+                </h3>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+
+      {/* Metrics */}
+      <div
+        className="
+          relative mx-auto
+          mt-[4px] w-[285px]
+          pl-[28px]
+
+          md:mx-0
+          md:-translate-y-3
+          md:w-auto
+          md:translate-x-20
+        "
+      >
+        {/* Moving dot */}
+        <span
+          aria-hidden="true"
+          className="
+            absolute left-0
+            h-[9px] w-[9px]
+            translate-y-10
+            rounded-full
+            bg-[#1434CB]
+            transition-transform
+            duration-500 ease-out
+          "
+          style={{
+            transform: `translateY(${activeSlide * 113}px)`,
+          }}
+        />
                 {platformSlides.map((slide, index) => {
                     const isActive = activeSlide === index
 
