e14281c2b9bc4cbe50253c61ef3073c780f55835
faseehullah404 <faseeh.ullah404@gmail.com>
2026-08-07 18:01:59 +0500

    Add task controller and route placeholders
    Create placeholder files for task management: server/src/controllers/taskcontrol.js and server/src/routes/taskroute.js. Update server.js to import the task model (model/task.js) and adjust console.log indentation. These files scaffold future task controller and routing implementation.

--- task-management-system/server/src/server.js
+++ task-management-system/server/src/server.js
@@ -1,6 +1,7 @@
 import "dotenv/config"
 import app from "./app.js"
 import connectDB from "./config/db.js"
+import task from "./model/task.js"
 
 const PORT = process.env.PORT || 5000
 
@@ -8,7 +9,7 @@ async function startServer() {
     await connectDB()
 
     app.listen(PORT, function () {
-        console.log(`Server running on http://localhost:${PORT}`)
+      console.log(`Server running on http://localhost:${PORT}`)
     })
 }
 
